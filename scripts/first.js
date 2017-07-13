5d9e305e72e2e6f11741ec44acdae975f93c5b6b
Juan Camilo Arroyave Rico <desarrolloweb@dynamicweb.co>
2017-07-13 16:22:50 -0500

    Primer commit

---
+++ scripts/first.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+/*
+Created by Juan Camilo Arroyave Rico
+This script is for open and close the sidebar menu
+only with an action with the open button in the navigation bar
+*/
+
+function openNav() {
+    document.getElementById("navigation").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("navigation").style.width = "0";
+}
