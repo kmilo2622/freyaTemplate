e6349043b3fe223ddb1eb60016d999c6d8b490ec
Juan Camilo Arroyave Rico <desarrolloweb@dynamicweb.co>
2017-07-14 11:32:44 -0500

    Escudo y favicon

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ Versión de Dreamweaver CC: 13.2 Build 6466
     <meta name="description" content="La Corporación Freya es una organización sin ánimo de lucro que buscar mejorar la calidad de vida de las personas">
     <!--Inserción de los estilos-->
     <!--Favicon: Ícono que se visualiza en la b arra de direcciones y pestañas del navegador-->
-    <link rel="icon" type="image/png" href="imagenes/escudo.png" />
+    <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/imagenes/escudo.png" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <?php wp_head(); ?>
 </head>
@@ -32,7 +32,7 @@ Versión de Dreamweaver CC: 13.2 Build 6466
         <header class="header">
             <!--Puede ser un poco confuso, pero el menú de navegación es similar a un sidebar así que éste es el estilo para alojar la barra delm header que es demasiado importante-->
             <div class="navbar">
-                <div id="brand"><img src="imagenes/logo.png" alt="Logotipo de la Corporación Freya" width="100px" height="50px"></div>
+                <div id="brand"><img src="<?php echo get_stylesheet_directory_uri(); ?>/imagenes/logo.png" alt="Logotipo de la Corporación Freya" width="100px" height="50px"></div>
                 <div id="burger" onclick="openNav()" style="font-size:40px;cursor:pointer">&#5816;</div>
             </div>
             <!-- Final del Header-->
