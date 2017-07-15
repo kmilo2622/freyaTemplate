05a6d9d267c8a6872229af151a754df3ab582c51
Juan Camilo Arroyave Rico <desarrolloweb@dynamicweb.co>
2017-07-15 11:16:44 -0500

    Mejoramiento significativo del index

--- template_index.php
+++ template_index.php
@@ -11,21 +11,93 @@ Template Name: Template Index
     <!--Final de la imagen de inicio-->
 </div>
 <div class="banner">
+    <br>
     <!--Inicio del banner-->
-    <p id="hero2">Conoce Aquí un Mundo Lleno de Oportunidades</p>
-    <div class="buttoncontainer"><a href="paginas/nosotros.html" class="btn1">Entrar</a></div>
+    <div class="container">
+        <div class="col-md-6">
+            <p id="hero2">Conoce Aquí un Mundo Lleno de Oportunidades</p>
+        </div>
+        <div class="col-md-6">
+            <div class="buttoncontainer"><a href="" class="btn1">Entrar</a></div>
+        </div>
+        <br>
+    </div>
     <!--Final del Banner-->
 </div>
 <div class="front-image2">
     <!--Si tuviera Bootstrap, éste sería el Jumbotron-->
+    <br><br><br>
     <p class="hero-text">Nuestros Servicios</p>
+    <div class="container">
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="">
+                    <div class="caption text-center">
+                        <h3>Cursos Online</h3>
+                    </div>
+                    <img src="http://identidad.queretaro.itesm.mx/wp-content/uploads/2014/10/banner-frases-diseno-web.jpg" class="img-circle" alt="Cinque Terre" width="200" height="400">
+                </a>
+                <div class="caption">
+                    <br>
+                    <!-- Opcional -->
+                    <p>
+                        Estudia a través de esta plataforma aquello que te dará un mejor futuro
+                        <br>
+                        <a href="">Leer Más...</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="">
+                    <div class="caption text-center">
+                        <h3>Desarrollo a Medida</h3>
+                    </div>
+                    <img src="http://www.tudocursosgratuitos.com/wp-content/uploads/2014/12/Cursos-T%C3%A9cnicos-IFMS-online.jpg" class="img-circle" alt="Cinque Terre" width="200" height="400">
+                </a>
+                <div class="caption">
+                    <br>
+                    <!-- Opcional -->
+                    <p>
+                        Aquí realizamos tu página software y páginas web con la mejor calidad.
+                        <br>
+                        <a href="">Leer Más...</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="">
+                    <div class="caption text-center">
+                        <h3>Diseño y Publicidad Empresarial</h3>
+                    </div>
+                    <img src="http://www.arqhys.com/arquitectura/fotos/arquitectura/Dise%C3%B1o-gr%C3%A1fico.jpg" class="img-circle" alt="Cinque Terre" width="200" height="400">
+                </a>
+                <div class="caption">
+                    <br>
+                    <!-- Opcional -->
+                    <p>
+                        Tu empresa puede tener una identidad
+                        <br>
+                        <a href="">Leer Más...</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
     <!--Final de la imagen de inicio-->
 </div>
 <section class="text1">
     <div id="text1" align="center">
-        <h2>Título</h2>
-        <p> lorem expands to Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, non, quos voluptas vel earum unde laboriosam qui optio asperiores nesciunt sed voluptatibus quae harum nisi iure sapiente numquam quo minus. </p>
-        <p><a href="paginas/servicios.html" class="btn1">Entrar</a></p>
+        <h2>¿Quiénes Somos?</h2>
+        <p> Somos una organización sin ánimo de lucro que buscar mejorar considerablemente la vida de las personas y crear un cambio social significativo a través de la apertura de oportunidades para las comunidades menos favorecidas en todo el mundo a través de Internet </p>
+        <br>
+        <p><a href="" class="btn1">Leer Más</a></p>
     </div>
 </section>
 <div id="contact"></div>
